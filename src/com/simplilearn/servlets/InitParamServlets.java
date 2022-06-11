34f8e7385566fcbf3622c9ce4b906f8559a27c66
Dhruvik <dhruviksparikh@gmail.com>
2022-06-11 22:13:42 +0530

    Added changes

---
+++ src/com/simplilearn/servlets/InitParamServlets.java
@@ -0,0 +1,52 @@
+package com.simplilearn.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InitParamServlets
+ */
+@WebServlet(urlPatterns = "/init-param", initParams = @WebInitParam(name = "name", value = "Simplilearn"))
+public class InitParamServlets extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InitParamServlets() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = getServletConfig().getInitParameter("name");
+		String location = getServletContext().getInitParameter("location");
+		PrintWriter out = response.getWriter();
+		out.println(name+"-");
+		out.println(location);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
